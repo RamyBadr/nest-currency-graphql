efdf6e4170749d2b7cc7a01b0145773782b20cbe
unknown <ramybadr@3ddx.com>
2020-03-23 00:58:39 +0200

    clean

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,16 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsModule } from './cats/cats.module';
+
 import { CurrencyModule } from './currency/currency.module';
 
 @Module({
   imports: [
-    // CatsModule,
     CurrencyModule,
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql',
     }),
-    // MongooseModule.forRoot('mongodb://localhost/nest'),
   ],
   controllers: [AppController],
   providers: [AppService],
